d5afcd52bcd3535603888b8bff21588f631a5942
mavan71 <mavan71@gmail.com>
2023-05-20 17:41:46 +0300

    18Mayıs

--- src/test/java/Day10/C02_JsExecutorCalisma.java
+++ src/test/java/Day10/C02_JsExecutorCalisma.java
@@ -2,6 +2,7 @@ package Day10;
 
 import Utilities.Base;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 
 public class C02_JsExecutorCalisma extends Base {
@@ -17,7 +18,7 @@ public class C02_JsExecutorCalisma extends Base {
     }
 
     @Test
-    public void scrollWithJs(){
+    public void scrollWithJs() throws InterruptedException {
         // Bunlar JS methodlari
 
         // scrollBy()
@@ -25,7 +26,17 @@ public class C02_JsExecutorCalisma extends Base {
         // scrollIntoView()
         driver.get("https://www.amazon.com/");
         JavascriptExecutor jse= (JavascriptExecutor) driver;
-        jse.executeScript("");
+        jse.executeScript("window.scrollBy(0,1500)");
+        Thread.sleep(3000);
+        jse.executeScript("window.scrollTo(0,0)");
+        jse.executeScript("window.scrollTo(0,document.body.scrollHeight/3)");
+        jse.executeScript("arguments[0].scrollIntoView()",driver.findElement(By.id("gw-sign-in-button")));
+        Thread.sleep(3000);
+        for (int i = 0; i < 50; i++) {
+            jse.executeScript("window.scrollBy(0,30)");
+            Thread.sleep(300);
+
+        }
     }
 
 
